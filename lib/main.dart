8cb2e21d2eea17b147456b9ede829308a58fb7b4
J Charles Adebola <writingjca@gmail.com>
2022-06-26 20:17:57 +0100

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:mealee/categories_screen.dart';
+import 'screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          //the primarySwatch now placed into this code
+          primary: Colors.pink,
+          //the new accent color code
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoCondensed'),
+            ),
+      ),
+      home: const CategoriesScreen(),
+      routes: {
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
+      },
     ),
-    home: CategoriesScreen(),
-  ));
+  );
 }
